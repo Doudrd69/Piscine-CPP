8fdc3fa00a6ddb162323c105d00a03d39b912bf6
Edouard Brodeur <ebrodeur@z1r8p1.42lyon.fr>
2023-02-10 17:15:26 +0100

    cpp06 ex00

--- CPP06/ex00/srcs/main.cpp
+++ CPP06/ex00/srcs/main.cpp
@@ -1,54 +1,87 @@
 #include <iostream>
 #include <cstdlib>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
+
+bool	check_isnan_isinf(double value) {
+
+	if (std::isnan(value) == true || std::isinf(value) == true)
+	{
+		std::cout << "Char: impossible" << std::endl;
+		std::cout << "Int: impossible" << std::endl;
+		std::cout << "Float: " << static_cast<float>(value) << "f" << std::endl;
+		std::cout << "Float: "  << value << std::endl;
+		return true;
+	}
+	return false;
+}
 
 int main(int argc, char **argv) {
 
     if (argc == 2)
     {
-        double      d;
-        float       f;
-        long		i;
-        char        *end;
+		char	*end;
+		std::string	tmp = argv[1];
+		int	tmp_length = tmp.length();
 
-        i = std::strtol(argv[1], &end, 10);
-        if (*end == 0) {
-            std::cout << "i is an int ! " << i << std::endl;
-            if (std::isprint(i) != 0)
-                std::cout << "Char: " << static_cast<char>(i) << std::endl;
-            else
-                std::cout << "Char: Non displayable" << std::endl;
-            std::cout << "Int: " << static_cast<int>(i) << std::endl;
-            std::cout << "Float: " << static_cast<float>(i) << std::endl;
-            std::cout << "Double: " << static_cast<double>(i) << std::endl;
-            return 0;
-        }
+		int	i_input = std::strtol(argv[1], &end, 10);
+		if (*end == 0) {
 
-        d = std::strtod(argv[1], &end);
-        if (*end == 0) {
-            std::cout << "d is a double ! " << d <<  std::endl;
-            d == d ? std::cout << "Its a real number" << std::endl : std::cout << "Its NaN" << std::endl;
-            if (std::isprint(d) != 0)
-                std::cout << "Char: " << static_cast<char>(d) << std::endl;
-            else
-                std::cout << "Char: Non displayable" << std::endl;
-            std::cout << "Int: " << static_cast<int>(d) << std::endl;
-            std::cout << "Float: " << static_cast<float>(d) << std::endl;
-            std::cout << "Double: " << static_cast<double>(d) << std::endl;
-            return 0;
-        }
+			std::cout << "i_input ! " << i_input << std::endl;
+			if (std::isprint(i_input))
+				std::cout << "Char: '" << static_cast<char>(i_input) << "'" << std::endl;
+			else
+				std::cout << "Char: non displayable" << std::endl;
+			std::cout << "Int: " << i_input << std::endl;
+			std::cout << "Float: " << std::fixed << std::setprecision(1) << static_cast<float>(i_input) << "f" << std::endl;
+			std::cout << "Double: " << static_cast<double>(i_input) << std::endl;
+			return 0;
+		}
 
-        f = std::strtof(argv[1], &end);
-        if (*end == 0) {
-            std::cout << "f is a float ! " << f << std::endl;
-            if (std::isprint(f) != 0)
-                std::cout << "Char: " << static_cast<char>(f) << std::endl;
-            else
-                std::cout << "Char: Non displayable" << std::endl;
-            std::cout << "Int: " << static_cast<int>(f) << std::endl;
-            std::cout << "Float: " << static_cast<float>(f) << std::endl;
-            std::cout << "Double: " << static_cast<double>(f) << std::endl;
-            return 0;
-        }
+		float	f_input = std::strtof(argv[1], &end);
+		int length = end - argv[1];
+		if ((length == tmp_length - 1) && *end == 'f' && *(end + 1) == '\0') {
+
+			if (check_isnan_isinf(f_input) == true)
+				return 1;
+			std::cout << "f_input ! " << f_input << std::endl;
+			if (std::isprint(f_input))
+				std::cout << "Char: '" << static_cast<char>(f_input) << "'" << std::endl;
+			else
+				std::cout << "Char: non displayable" << std::endl;
+			std::cout << "Int: " << static_cast<int>(f_input) << std::endl;
+			std::cout << "Float: " << std::fixed << std::setprecision(1) << f_input << "f" << std::endl;
+			std::cout << "Double: " << static_cast<double>(f_input) << std::endl;
+			return 0;
+		}
+
+		double	d_input = std::strtod(argv[1], &end);
+		if (*end == 0) {
+
+			if (check_isnan_isinf(d_input) == true)
+				return 1;
+			std::cout << "d_input ! " << d_input << std::endl;
+			if (std::isprint(d_input))
+				std::cout << "Char: '" << static_cast<char>(d_input) << "'" << std::endl;
+			else
+				std::cout << "Char: non displayable" << std::endl;
+			std::cout << "Int: " << static_cast<int>(d_input) << std::endl;
+			std::cout << "Float: " << std::fixed << std::setprecision(1) << static_cast<float>(d_input) << "f" << std::endl;
+			std::cout << "Double: " << d_input << std::endl;
+			return 0;
+		}
+
+		if (argv[1][0] && !argv[1][1]) {
+
+			std::cout << "char_input ! " << argv[1] << std::endl;
+			std::cout << "Char: '" << argv[1][0] << "'" << std::endl;
+			std::cout << "Int: " << static_cast<int>(argv[1][0]) << std::endl;
+			std::cout << "Float: " << std::fixed << std::setprecision(1) << static_cast<float>(argv[1][0]) << "f" << std::endl;
+			std::cout << "Double: " << static_cast<double>(argv[1][0]) << std::endl;
+			return 0;
+		}
+
         std::cout << "Argument should be a scalar type !";
         std::cout << " --> char / int / float / double" << std::endl;
         return 1;
@@ -56,12 +89,3 @@ int main(int argc, char **argv) {
     std::cout << "Error : wrong number of arguments : only one is requested" << std::endl;
     return 1;
 }
-
-//types scalaires suivants : char, int, float ou double
-    //avec typeid on obtient c / i / f / d
-//regarder du cote de sstream
-
-// 1-détecter le type du littéral passé en paramètre
-    // 2-le convertir de sa représentation sous forme de chaîne de caractères
-    //vers son véritable type
-    // 3-le convertir explicitement vers les trois autres types de données
